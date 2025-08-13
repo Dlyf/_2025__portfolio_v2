e937ce9431adee3fca77883049156a2947e37435
Dlyf <domilung@gmail.com>
2025-08-13 10:06:48 +0200

    Initial commit of portfolioV2

---
+++ my-portfoliov2/src/app/components/Section.tsx
@@ -0,0 +1,13 @@
+
+import { cn } from "@/lib/utils"
+import { PropsWithChildren } from "react"
+
+const Section = (props: PropsWithChildren<{className?:string}>) => {
+  return (
+    <section className={cn("max-w-3xl px-4 m-auto")}>
+        {props.children}
+    </section>
+  )
+}
+
+export default Section
