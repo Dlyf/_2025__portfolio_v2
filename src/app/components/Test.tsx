af063bebeb39743f1161914ee8a3712fc260ef8d
Dlyf <domilung@gmail.com>
2025-08-13 10:46:51 +0200

    test lorem

--- src/app/components/Test.tsx
+++ src/app/components/Test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Test = () => {
   return (
-    <div>Test</div>
+    <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit fuga iure ab doloribus reprehenderit voluptate nobis atque illo minus omnis voluptatum, sint aliquam in sapiente assumenda numquam. Magnam, aliquam earum?</div>
   )
 }
 
